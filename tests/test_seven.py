73c8054128e6519ec14d2f10ffbc01afae29f02b
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-17 22:34:54 -0500

    Add tests for phone number, groups return

--- tests/test_seven.py
+++ tests/test_seven.py
@@ -1,6 +1,7 @@
 """Test file for seven.py file using pytest
 """
 from src.chapters.seven.seven import search_phone_numbers
+from src.chapters.seven.seven import search_for
 
 
 def test_search_phone_numbers_one():
@@ -15,3 +16,32 @@ def test_search_phone_numbers_two():
     find = r'\d{3}-\d{3}-\d{4}'
     found = search_phone_numbers(find, test_str)
     assert found.group() == '123-555-2357'
+
+
+def test_search_phone_numbers_three():
+    test_str = 'My phone is (123) 555-2357 call soon'
+    find = r'\(\d{3}\)\s(\d{3}-\d{4})'
+    found = search_phone_numbers(find, test_str)
+    assert found.group() == '(123) 555-2357'
+
+
+def test_search_phone_numbers_four():
+    test_str = 'My phone is (123) 555-2357 call soon'
+    find = r'\(\d{3}\)\s(\d{3}-\d{4})'
+    found = search_phone_numbers(find, test_str)
+    assert found.group(0) == '(123) 555-2357'
+
+
+def test_search_phone_numbers_five():
+    test_str = 'My phone is (123) 555-2357 call soon'
+    find = r'(\(\d{3}\))\s(\d{3}-\d{4})'
+    found = search_phone_numbers(find, test_str)
+    assert found.group(1) == '(123)'
+
+
+def test_search_phone_numbers_five():
+    test_str = 'My phone is (123) 555-2357 call soon'
+    find = r'(\(\d{3}\))\s(\d{3}-\d{4})'
+    found = search_phone_numbers(find, test_str)
+    assert found.group(2) == '555-2357'
+
