d00187e4cc4c243bce0df01a2f18e9b2e0b7254d
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-17 22:27:12 -0500

    Add two tests (ssl)
    Test: \d\d\d-\d\d\d-\d\d\d\d
    Test: \d{3}-\d{3}-\d{4}

--- tests/test_seven.py
+++ tests/test_seven.py
@@ -1,2 +1,17 @@
 """Test file for seven.py file using pytest
 """
+from src.chapters.seven.seven import search_phone_numbers
+
+
+def test_search_phone_numbers_one():
+    test_str = 'My phone is 123-555-2357 call soon'
+    find = r'\d\d\d-\d\d\d-\d\d\d\d'
+    found = search_phone_numbers(find, test_str)
+    assert found.group() == '123-555-2357'
+
+
+def test_search_phone_numbers_two():
+    test_str = 'My phone is 123-555-2357 call soon'
+    find = r'\d{3}-\d{3}-\d{4}'
+    found = search_phone_numbers(find, test_str)
+    assert found.group() == '123-555-2357'
