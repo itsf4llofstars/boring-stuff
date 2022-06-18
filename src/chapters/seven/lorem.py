36400e7492e0cb9ba25d82e05d9cbeb6a2101c48
itsf4llofstars <irooted4hal@mailfence.com>
2022-06-17 19:52:51 -0500

    Add setup up lorem text and lorem py files

---
+++ src/chapters/seven/lorem.py
@@ -0,0 +1,64 @@
+lorem_text = (
+    'Adipisicing sit atque doloremque eveniet ducimus maiores Labore perferendi\n',
+    'voluptas rem atque incidunt Rerum maxime repellendus veritatis odit cupiditate\n',
+    'necessitatibus At omnis modi optio officiis perferendis sequi Illo accusamus\n',
+    'aliquid quisquam sint veritatis iste, facilis! Totam ab veniam est nemo tenetu!\n',
+    'Molestias totam nemo quisquam eligendi inventore? Nobis accusamus eveniet ea\n',
+    'repudiandae dignissimos Architecto sapiente soluta aperiam quibusdam sed Totam\n',
+    'itaque ad suscipit impedit a est. Eius vero eveniet deserunt magnam adipisci\n',
+    'Doloribus quidem aperiam optio rem minima? Exercitationem ullam est expedita\n',
+    'odio exercitationem consequatur Consectetur ab voluptate autem iste blanditiis\n',
+    'consectetur. Quae ducimus exercitationem rem aperiam ullam reprehenderit.\n',
+    'Excepturi qui eveniet laborum ut eligendi, mollitia cupiditate Error quis\n',
+    'doloribus quia alias recusandae Quae voluptatum at eum distinctio placeat illo\n',
+    'perspiciatis. Explicabo quibusdam vero corrupti autem porro! Exercitationem\n',
+    'odio consequatur eius unde obcaecati Iste sed esse adipisci quia corrupti\n',
+    'quibusdam Nulla vitae modi eius fuga maxime Id modi temporibus commodi tempora\n',
+    'doloribus atque Sequi aspernatur aperiam reprehenderit earum fugit Corporis\n',
+    'aut at laborum quis earum id exercitationem porro, consequatur excepturi nam\n',
+    'eos architecto odio modi Voluptates consectetur corrupti autem ullam similiqu\n',
+    '? Odit labore similique nam voluptatum amet! Alias repudiandae culpa\n',
+    'laboriosam aut architecto! Facilis vel voluptatibus cupiditate similique eius\n',
+    'qui.\n',
+    '\tConsectetur ea itaque similique atque officiis. Maiores iure quod numquam\n',
+    'quod asperiores. Suscipit dolor magni rerum architecto corporis Exercitationem\n',
+    'dolore voluptatibus quaerat temporibus architecto! Deleniti assumenda earum\n',
+    'fuga aliquam eligendi? Beatae placeat molestiae aliquid at sapiente iusto.\n',
+    'Repellendus vitae nobis quos deserunt sit Nesciunt recusandae sed obcaecati\n',
+    'tenetur quas consequuntur beatae esse accusantium Facilis ullam nesciunt in\n',
+    'nostrum possimus, exercitationem Aspernatur cum id quo iure esse. Amet iste\n',
+    'molestiae libero voluptatibus quo. Sequi nostrum eligendi nemo impedit fugit.\n',
+    'Porro dolores molestiae quas sapiente rem? Consectetur deserunt odio quibusdam\n',
+    'quod quod, itaque Est nobis doloribus molestias impedit blanditiis obcaecati\n',
+    'Repudiandae quo\n',
+    '\tSit maiores recusandae ipsum nihil amet iste. Et illo at nulla quaerat\n'
+    'assumenda. Quis mollitia maiores praesentium corrupti laboriosam placeat\n'
+    'voluptatem? Dolore repellat commodi accusantium expedita incidunt Quod\n'
+    'possimus ex fugiat iusto et nostrum. Mollitia repellendus ut nostrum molestias\n'
+    'saepe Eaque quibusdam sapiente nihil facere ut. Aspernatur voluptas quod omnis\n'
+    'ratione recusandae Vitae odit ipsam distinctio eius excepturi aspernatur ad!\n'
+    'Illum dicta consequuntur velit dignissimos nisi. Quis fugit quo dolorem\n'
+    'asperiores nulla. Impedit aspernatur eligendi dolorem doloremque qui quisquam!\n'
+    'Numquam ipsam quis est reprehenderit ipsa. Commodi culpa fugit dolorem at\n'
+    'distinctio corrupti. Quisquam sequi odio reprehenderit quidem in Assumenda\n'
+    'distinctio repellendus aliquam libero rerum. Voluptas excepturi laboriosam\n'
+    'ratione rem necessitatibus Ex corporis soluta ab quibusdam amet rem! Ducimus\n'
+    'laborum inventore iste adipisci quae vero culpa. Eveniet ea architecto\n'
+    'reiciendis repellat commodi? Velit ab sequi magnam aperiam earum. Aspernatur\n'
+    'est minus deserunt inventore provident Vel accusantium illo autem aliquam\n'
+    'voluptas quis esse, omnis. Cupiditate hic nesciunt rem nostrum est quaerat Id\n'
+    'unde sed culpa recusandae voluptate? Ea consectetur dicta dolore excepturi\n'
+    'dignissimos? Dolores iste quisquam dolor dignissimos obcaecati. Doloribus sint\n'
+    'quas harum qui eum explicabo A pariatur ipsa placeat laudantium eveniet\n'
+    'Ducimus alias nam dolorem nemo molestias. Dolorem perspiciatis voluptatem\n'
+    'accusamus repellendus dolores Velit beatae nostrum earum ad laboriosam ad Hic\n'
+    'et nemo suscipit libero laboriosam Saepe magnam harum expedita ducimus culpa\n'
+    'minima odit nihil, est. Minus recusandae vero nobis libero quidem? Perferendis\n'
+    'vel nihil dolore libero quaerat iure laudantium. In culpa minus odio libero\n'
+    'expedita? Incidunt qui odio praesentium perferendis delectus. Consectetur rem\n'
+    'perferendis veniam possimus iure perspiciatis! Autem animi doloremque at\n'
+    'voluptas minus Aspernatur totam molestias esse dolores perferendis. Numquam\n'
+    'voluptatibus soluta aut ipsa amet In tenetur aliquid id quos excepturi Eius\n'
+    'architecto libero deserunt suscipit porro sunt Molestiae quisquam suscipit\n'
+    'dolore ipsa aperiam Similique rem iste quas modi\n'
+)
